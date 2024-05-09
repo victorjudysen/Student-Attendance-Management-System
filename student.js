67747d9cdb0ce2ab3f5eeff15bb5d3321c79c952
victorjudysen <117171668+victorjudysen@users.noreply.github.com>
2024-05-09 23:54:14 +0300

    Update student.js

--- student.js
+++ student.js
@@ -6,15 +6,15 @@ function submitFun1(e) {
     document.querySelector("#tbody").innerHTML = "";
     e.preventDefault();
     var name = document.querySelector("#name").value;
-    var number = document.querySelector("#number").value;
-    var city = document.querySelector("#city").value;
-    var rollNo = document.querySelector("#rollNo").value;
+    var register = document.querySelector("#regNo").value;
+    var degree = document.querySelector("#degree").value;
+    // var rollNo = document.querySelector("#rollNo").value;
 
     var studentObj = {
         name: name,
-        number: number,
-        city: city,
-        rollNo: rollNo
+        register: regNo,
+        degree: degree,
+        // rollNo: rollNo
     }
 
     studentDataArr.push(studentObj);
@@ -34,30 +34,37 @@ function displayFun(studentDataArr) {
 
         var td1 = document.createElement("td");
         td1.innerHTML = count++
+
         var td2 = document.createElement("td");
         td2.innerHTML = item.name;
+
         var td3 = document.createElement("td");
-        td3.innerHTML = item.number;
+        td3.innerHTML = item.register;
+
         var td4 = document.createElement("td");
-        td4.innerHTML = item.city;
-        var td5 = document.createElement("td");
-        td5.innerHTML = item.rollNo;
+        td4.innerHTML = item.degree;
+
+        // var td5 = document.createElement("td");
+        // td5.innerHTML = item.rollNo;
+
         var td6 = document.createElement("td");
+
         var btn1 = document.createElement("button");
         btn1.innerHTML = "P";
         btn1.addEventListener("click", function () {
             td6.innerHTML = "<button >Present</button>";
         });
+
         var btn2 = document.createElement("button");
         btn2.innerHTML = "A";
         btn2.addEventListener("click", function () {
             td6.innerHTML = "<button id='absent'>Absent</button>";
-        
         });
+
         td6.classList.add("td6");
         td6.append(btn1, btn2);
 
-        tr.append(td1, td2, td3, td4, td5, td6);
+        tr.append(td1, td2, td3, td4, td6);
 
         document.querySelector("#tbody").append(tr);
 
